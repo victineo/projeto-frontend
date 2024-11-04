8809218e33fff65e6f5dc08434f96721113e4c88
Victor <victorhenrique.alves.lopes@gmail.com>
2024-11-04 14:43:13 -0300

    Modificando as funções do CRUD

--- src/controllers/controller_tarefas.js
+++ src/controllers/controller_tarefas.js
@@ -15,7 +15,7 @@ export async function getAllTasks() {
 }
 
 // Listar uma tarefa específica
-export async function getTask() {
+export async function getTask(id) {
     const task = tasks.find(task => task.id === id);
     return Promise.resolve(task);
 }
@@ -25,6 +25,18 @@ export async function updateTask(id, updatedFields) {
     const task = await getTask(id);
 
     if (task) {
-        
+        Object.assign(task, updatedFields);
+        return Promise.resolve(task);
     }
+    return Promise.resolve(null);
+}
+
+// Deletar uma tarefa
+export async function deleteTask(id) {
+    const index = tasks.findIndex(task => task.id === id);
+    if (index !== -1) {
+        const deletedTask = tasks.splice(index, 1)[0];
+        return Promise.resolve(deletedTask);
+    }
+    return Promise.resolve(null);
 }
