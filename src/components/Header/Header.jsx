152925274650ee2d80575408549f3d1da26891c2
Victor <victorhenrique.alves.lopes@gmail.com>
2024-11-04 19:38:51 -0300

    Substituindo o logotipo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,21 @@
 import styles from "./Header.module.css";
 import { NavBar } from "../NavBar/NavBar.jsx";
-import Logo from "../../assets/react.svg";
+import Logo from "../../assets/check-fat-fill.svg";
 import { InputField } from "../InputField/InputField.jsx";
 import { MagnifyingGlass } from "phosphor-react";
 
 export function Header() {
   return (
     <header>
-        <div>
-            <img src={Logo} alt="Logo" />
-            <h1>To-do list</h1>
-        </div>
-            <NavBar />
-            <InputField 
-                placeholder="Adicione uma nova tarefa"
-                icon={<MagnifyingGlass size={24} />}
-            />
+      <div>
+        <img className={styles.logo} src={Logo} alt="Logo" />
+        <h1>To-do list</h1>
+      </div>
+      <NavBar />
+      <InputField 
+        placeholder="Adicione uma nova tarefa"
+        icon={<MagnifyingGlass size={24} />}
+      />
     </header>
   );
 }
