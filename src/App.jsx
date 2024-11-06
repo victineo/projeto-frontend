694e04d5732e7a48653e9fe1c72a8cfb1c21905b
Victor <victorhenrique.alves.lopes@gmail.com>
2024-11-06 16:59:37 -0300

    Ajustando detalhes da estilização da interface

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import { Aside } from './components/Aside/Aside'
 import { Task } from './components/Task/Task'
 import { Plus } from 'phosphor-react'
 import { ProgressBar } from './components/ProgressBar/ProgressBar'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
+    
       <Aside />
       <div className='container'>
         <button className='createTaskButton'> <Plus size={24} />Criar nova tarefa</button>
--- src/routes/LoginPage.jsx
+++
@@ -1,7 +0,0 @@
-import {Login} from "../components/Login/Login.jsx"
-
-export function LoginPage(){
-    return (
-        <Login/>
-    )
-}
