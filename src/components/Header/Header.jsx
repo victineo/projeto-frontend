2c2686ced454f127590aa1ce42f4e169a8ffc7fc
Victor <victorhenrique.alves.lopes@gmail.com>
2024-11-07 20:52:14 -0300

    Melhorando a estilização do Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,15 @@
-import { NavBar } from "../NavBar/NavBar"
+import { NavBar } from "../NavBar/NavBar";
 import Logo from "../../assets/check-fat-fill.svg";
-import styles from "./Header.module.css"
+import styles from "./Header.module.css";
 
 export function Header() {
     return (
-        <div>
+        <div className={styles.headerContainer}>
             <div className={styles.logoContainer}>
                 <img className={styles.logo} src={Logo} alt="Logo" />
                 <h1>To-do list</h1>
             </div>
-            <NavBar />
+            <NavBar orientation="horizontal" />
         </div>
-    )
+    );
 }
