7d427ecce0c4316c8db1f7e98244cb43afc165ab
Victor <victorhenrique.alves.lopes@gmail.com>
2024-11-07 20:50:09 -0300

    Adicionando props de orientação à NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,10 +2,10 @@ import styles from "./NavBar.module.css";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
-export function NavBar() {
+export function NavBar({ orientation = "vertical" }) {
   return (
-    <nav>
-      <ul>
+    <nav className={styles.navBar}>
+      <ul className={orientation === "vertical" ? styles.vertical : styles.horizontal}>
         <Link to="/subscriptions" className={styles.navLink}>Planos de assinatura</Link>
         <Link to="/about-us" className={styles.navLink}>Sobre nós</Link>
         <Link to="/help" className={styles.navLink}>Ajuda</Link>
