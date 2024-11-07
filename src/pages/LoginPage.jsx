de0d068bba2a62ca9a26c160efd4b07c654b954a
Victor Henrique <victorhenrique.alves.lopes@gmail.com>
2024-11-07 10:37:01 -0300

    Inserindo conteúdo na página de Login

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,18 @@
+import { Login } from "../components/Login/Login"
+import styles from "./LoginPage.module.css"
+import { ArrowLeft } from "phosphor-react";
+import { Link } from "react-router-dom";
+import { Header } from "../components/Header/Header";
+import { ButtonBack } from "../components/ButtonBack/ButtonBack";
+
 export function LoginPage() {
     return (
         <div>
-            <h1>Login Page</h1>
+            <Header />
+            <ButtonBack />
+            <div className={styles.loginPage}>
+                <Login />
+            </div>
         </div>
     );
 }
