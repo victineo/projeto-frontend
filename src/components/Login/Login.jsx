62ab4a0bc4271b9642c03fcea7381b7837f4f878
Victor Henrique <victorhenrique.alves.lopes@gmail.com>
2024-11-07 10:29:32 -0300

    Melhorando a estilização do Login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,54 +1,36 @@
 import { useState } from "react";
-// import { FaUser, FaLock } from "phosphor-react";
 import styles from "./Login.module.css";
 
 export function Login () {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleSubmit = (event) => {
-
-    event.preventDefault();
-
-    console.log("Dados de Login:", { username, password });
-  };
-
   return (
     <div className={styles.container}>
-      <form onSubmit={handleSubmit}>
-        <h1>Acesse o sistema</h1>
+      <form>
+        <h1>Faça login</h1>
         <div className={styles.inputField}>
           <input
             type="text"
             placeholder="E-mail"
             required
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
           />
-          <FaUser className={styles.icon} />
         </div>
         <div className={styles.inputField}>
           <input
             type="password"
             placeholder="Senha"
             required
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
           />
-          <FaLock className={styles.icon} />
         </div>
-
-        <div className={styles.recall-forget}>
+        <div className={styles.recallForget}>
           <label>
             <input type="checkbox" />
             Lembre de mim
           </label>
           <a href="#">Esqueceu sua senha?</a>
         </div>
-        <button type="submit">Login</button>
-        <div className={styles.signup-link}>
+        <button className={styles.loginButton} type="submit">Login</button>
+        <div className={styles.signupLink}>
           <p>
-            Não tem uma conta? <a href="#">Registar</a>{" "}
+            Nao tem uma conta? <a href="#">Registar</a>{" "}
           </p>
         </div>
       </form>
