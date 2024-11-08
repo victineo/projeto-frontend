542497b124af1b18f35fe079edb4664811ad452c
Victor Henrique <victorhenrique.alves.lopes@gmail.com>
2024-11-08 10:46:11 -0300

    Melhorando a estilização do Register

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -4,11 +4,11 @@ export function Register() {
   return (
     <div className={styles.container}>
       <form className={styles.registerForm}>
-        <h1>Registro</h1>
+        <h1 className={styles.formTitle}>Registro</h1>
         <div className={styles.inputField}>
             <input 
                 type="text" 
-                placeholder="Nome"
+                placeholder="Nome Completo"
                 required
             />
         </div>
@@ -28,7 +28,7 @@ export function Register() {
         </div>
         <button className={styles.signInButton} type="submit">Registrar-se</button>
         <div className={styles.loginLink}>
-            <p>Ja possui uma conta? <a href="#">Login</a></p>
+            <p>Já possui uma conta? <a href="#">Faça login</a></p>
         </div>
       </form>
     </div>
