0e6b8e21f162428082122b919fc3f33a955a8714
Victor <victorhenrique.alves.lopes@gmail.com>
2024-11-08 23:44:20 -0300

    Começando a implementar useState para interações com o componente

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,11 +1,19 @@
+import { useState } from 'react';
 import styles from './Task.module.css'
+import { Circle } from 'phosphor-react';
 import { CheckCircle } from 'phosphor-react';
 
 export function Task({ task, onDelete }) {
+    const [isCompleted, setIsCompleted] = useState(false);
+
+    const handleCheckboxClick = () => {
+        setIsCompleted(!isCompleted);
+    };
+
     return (
         <div className={styles.taskCard}>
-            <div className={styles.checkBox}>
-                <CheckCircle size={24}/>
+            <div className={`${styles.checkBox} ${isCompleted ? styles.checked : ''}`} onClick={handleCheckboxClick}>
+                {isCompleted ? <CheckCircle size={24} weight="fill" /> : <Circle size={24} />}
             </div>
             <div className={styles.taskInfo}>
                 <div className={styles.taskTitle}>
