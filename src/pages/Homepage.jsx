921fd1456d922a2a9bef78aff94fcd38b0c987ef
Victor <victorhenrique.alves.lopes@gmail.com>
2024-11-11 00:06:13 -0300

    Tornando o processo de adição de tarefas dinâmico e interativo

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -4,32 +4,30 @@ import { Aside } from "../components/Aside/Aside";
 import { Task } from "../components/Task/Task";
 import { NewTask } from "../components/NewTask/NewTask";
 
-const tasks = [
-  { id: 1, title: "Estudar React", description: "Estudar conceitos de componentes, hooks e props", isCompleted: false },
-  { id: 2, title: "Criar um portfólio", description: "Criar um portfólio com React", isCompleted: false },
-];
-
 export function HomePage() {
-  const [newTask, setNewTask] = useState("");
+  const [tasks, setTasks] = useState([
+    { id: 1, title: "Estudar React", description: "Estudar conceitos de componentes, hooks e props", isCompleted: false },
+    { id: 2, title: "Criar um portfólio", description: "Criar um portfólio com React", isCompleted: false },
+  ]);
 
-  function handleCreateNewTask() {
+  function handleCreateNewTask(task) {
     event.preventDefault();
-
-    const task = {
-      id: Date.now(),
-      title: newTask,
-      isCompleted: false,
+    const newTask = {
+      id: tasks.length + 1,
+      title: task.title,
+      description: task.description,
+      isCompleted: false
     };
-    setTasks([...tasks, task]);
-    setNewTask("");
+
+    if (newTask.title.trim()) {
+      setTasks([...tasks, newTask]);
+    }
   }
   return (
     <>
       <Aside />
       <div className="container">
-        <NewTask
-          onCreate={handleCreateNewTask}
-        />
+        <NewTask onCreate={handleCreateNewTask} />
         <div className="taskList">
           {tasks.map(task => {
             return (
@@ -40,9 +38,6 @@ export function HomePage() {
             />
             );
           })}
-          <Task />
-          <Task />
-          <Task />
         </div>
       </div>
     </>
