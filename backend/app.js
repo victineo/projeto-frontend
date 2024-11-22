1023ac6171218ba9da40889e13ffeb6b1f104a13
Victor <victorhenrique.alves.lopes@gmail.com>
2024-11-22 18:35:25 -0300

    Corrigindo a importação da rota de usuários

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const userRoutes = require('./routes/users');
+const userRoutes = require('./routes/route_users');
 
 const app = express();
 
