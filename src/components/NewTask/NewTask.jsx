d3c7802c319e1e394dca9334f3858ddd3742a269
Victor <victorhenrique.alves.lopes@gmail.com>
2024-11-24 01:54:25 -0300

    Formatando a data antes de enviá-la para o backend

--- src/components/NewTask/NewTask.jsx
+++ src/components/NewTask/NewTask.jsx
@@ -1,68 +1,70 @@
-import React, { useState } from 'react';
-import styles from './NewTask.module.css';
-import axios from 'axios';
+import React, { useState } from "react";
+import styles from "./NewTask.module.css";
+import axios from "axios";
 
 export function NewTask({ onCreate }) {
-    const [title, setTitle] = useState('');
-    const [description, setDescription] = useState('');
-    const [date, setDate] = useState('');
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [date, setDate] = useState("");
 
-    async function handleCreateNewTask(event) {
-        event.preventDefault();
-        try {
-            const response = await axios.post('http://localhost:8000/api/tasks/add', {
-                title,
-                description,
-                date,
-            });
-            console.log('Tarefa criada com sucesso:', response.data.task);
-            onCreate(response.data); 
-        } catch (error) {
-            console.error('Detalhes do erro:', error);
-        }
+  async function handleCreateNewTask(event) {
+    event.preventDefault();
+
+    const [ano, mes, dia] = date.split("-");
+    const formattedDate = `${dia}/${mes}/${ano}`;
+
+    try {
+      const response = await axios.post("http://localhost:8000/api/tasks/add", {
+        title,
+        description,
+        date: formattedDate,
+      });
+      console.log("Tarefa criada com sucesso:", response.data.task);
+      onCreate(response.data);
+    } catch (error) {
+      console.error("Detalhes do erro:", error);
     }
-    
-    
+  }
 
-    const autoResize = (e) => {
-        e.target.style.height = 'auto';
-        e.target.style.height = `${e.target.scrollHeight}px`;
-    };
+  const autoResize = (e) => {
+    e.target.style.height = "auto";
+    e.target.style.height = `${e.target.scrollHeight}px`;
+  };
 
-    return (
-        <form className={styles.newTaskForm} onSubmit={handleCreateNewTask}>
-            <div className={styles.newTaskContent}>
-                <div className={styles.checkBox}>
-
-                </div>
-                <div className={styles.newTaskInfo}>
-                    <input
-                        className={styles.newTaskTitle}
-                        type="text"
-                        placeholder="Título da tarefa"
-                        value={title}
-                        onChange={(e) => setTitle(e.target.value)}
-                        onInvalid={(e) => e.target.setCustomValidity('A tarefa precisa ter um título!')}
-                        required
-                    />
-                    <textarea
-                        className={styles.newTaskDescription}
-                        placeholder="Descrição da tarefa"
-                        value={description}
-                        onChange={(e) => setDescription(e.target.value)}
-                        onInput={(e) => autoResize(e)}
-                    />
-                    <input
-                        className={styles.newTaskDate}
-                        type="date"
-                        value={date}
-                        onChange={(e) => setDate(e.target.value)}
-                        required
-                    />
-
-                </div>
-                <button type="submit" disabled={title.trim() === ''}>Criar</button>
-            </div>
-        </form>
-    );
+  return (
+    <form className={styles.newTaskForm} onSubmit={handleCreateNewTask}>
+      <div className={styles.newTaskContent}>
+        <div className={styles.checkBox}></div>
+        <div className={styles.newTaskInfo}>
+          <input
+            className={styles.newTaskTitle}
+            type="text"
+            placeholder="Título da tarefa"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            onInvalid={(e) =>
+              e.target.setCustomValidity("A tarefa precisa ter um título!")
+            }
+            required
+          />
+          <textarea
+            className={styles.newTaskDescription}
+            placeholder="Descrição da tarefa"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            onInput={(e) => autoResize(e)}
+          />
+          <input
+            className={styles.newTaskDate}
+            type="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
+        </div>
+        <button type="submit" disabled={title.trim() === ""}>
+          Criar
+        </button>
+      </div>
+    </form>
+  );
 }
