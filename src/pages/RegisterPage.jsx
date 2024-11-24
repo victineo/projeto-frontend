847ffb3a7eccea143022c28791ad7aa455876b52
Victor <victorhenrique.alves.lopes@gmail.com>
2024-11-24 19:32:27 -0300

    Removendo o botão de voltar da página agora que ele está no formulário

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,13 +1,11 @@
 import styles from "./RegisterPage.module.css";
 import { Register } from "../components/Register/Register";
-import { ButtonBack } from "../components/ButtonBack/ButtonBack";
 import { Header } from "../components/Header/Header";
 
 export function RegisterPage() {
   return (
     <div className={styles.wrapper}>
       <Header />
-      <ButtonBack />
       <div className={styles.registerPage}>
         <Register />
       </div>
