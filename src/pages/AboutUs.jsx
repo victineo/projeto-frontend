c68ac1711e529b08e4ca236a5ee3464ca34b0d1e
Victor <victorhenrique.alves.lopes@gmail.com>
2024-11-24 02:12:52 -0300

    Corrigindo a estilização da página

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -3,25 +3,27 @@ import { Header } from "../components/Header/Header";
 
 export function AboutUs() {
   return (
-    <div className={styles.aboutUs}>
+    <div className={styles.wrapper}>
       <Header />
-      <h1 className={styles.contentHeader}>Sobre Nós</h1>
-      <p>
-        Olá, Somos uma equipe de alunos cursando Análise e Desenvolvimento de
-        Sistemas e futuros desenvolvedores. Fizemos este projeto com o intuito
-        de ser entregue como trabalho da faculdade, porém esperamos que possam
-        fazer bom uso dele no dia a dia de vocês!
-      </p>
+      <div className={styles.content}>
+        <h1 className={styles.contentHeader}>Sobre Nós</h1>
+        <p>
+          Olá, Somos uma equipe de alunos cursando Análise e Desenvolvimento de
+          Sistemas e futuros desenvolvedores. Fizemos este projeto com o intuito
+          de ser entregue como trabalho da faculdade, porém esperamos que possam
+          fazer bom uso dele no dia a dia de vocês!
+        </p>
 
-      <h1 className={styles.contentHeader}>Gerente do Projeto</h1>
+        <h2 className={styles.title}>Gerente do Projeto</h2>
 
-      <li>Yure Dias dos Santos</li>
+        <li>Yure Dias dos Santos</li>
 
-      <h1 className={styles.contentHeader}>Desenvolvedores</h1>
-      <li>Victor Henrique Alves de Oliveira Lopes</li>
-      <li>Pietro Vianna da Rocha</li>
-      <li>Gustavo Drumond Duarte</li>
-      <li>Guilherme Pereira da Silva Licati de Queiroz</li>
+        <h2 className={styles.title}>Desenvolvedores</h2>
+        <li>Victor Henrique Alves de Oliveira Lopes</li>
+        <li>Pietro Vianna da Rocha</li>
+        <li>Gustavo Drumond Duarte</li>
+        <li>Guilherme Pereira da Silva Licati de Queiroz</li>
+      </div>
     </div>
   );
 }
